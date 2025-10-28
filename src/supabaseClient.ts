cf996cf394c6f6716d25f2c37ea46ecc3e27645f
Gustavo JrNdatipo <113350810+Decastro95@users.noreply.github.com>
2025-10-28 10:47:49 +0200

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,18 +1,6 @@
-// src/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from "./types/supabase";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing VITE_SUPABASE_URL or VITE_SUPABASE_ANON_KEY");
-}
-
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: true,
-  },
-});
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
